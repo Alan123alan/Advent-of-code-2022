e15694be7a74f0cbecd9bccb08857fac85190a0f
Alan123alan <alancastellanos2@hotmail.com>
2023-05-15 21:16:12 -0600

    Solved part 2 of day 9 with JS and NodeJS runtime, sometimes problems seem harder to solve in your head, just try them in the code and go from there

--- JS/day9/helpers.js
+++ JS/day9/helpers.js
@@ -3,6 +3,14 @@ const moveHead = (motions)=>{
     let tailMovements = [];
     let head = {x:0, y:0};
     let tail = {x:0, y:0};
+    let tail2 = {x:0, y:0};
+    let tail3 = {x:0, y:0};
+    let tail4 = {x:0, y:0};
+    let tail5 = {x:0, y:0};
+    let tail6 = {x:0, y:0};
+    let tail7 = {x:0, y:0};
+    let tail8 = {x:0, y:0};
+    let tail9 = {x:0, y:0};
     for(const motion of motions){
         const [direction, movements] = motion;
         for(let i=0; i<parseInt(movements); i++){
@@ -22,8 +30,16 @@ const moveHead = (motions)=>{
             }
             headMovements.push({x:head.x, y:head.y})
             tail = moveTail(head, tail)
-            if(!tailMovements.filter(tailMovement=>tailMovement.x==tail.x && tailMovement.y==tail.y).length>0){
-                tailMovements.push(tail)
+            tail2 = moveTail(tail, tail2)
+            tail3 = moveTail(tail2, tail3)
+            tail4 = moveTail(tail3, tail4)
+            tail5 = moveTail(tail4, tail5)
+            tail6 = moveTail(tail5, tail6)
+            tail7 = moveTail(tail6, tail7)
+            tail8 = moveTail(tail7, tail8)
+            tail9 = moveTail(tail8, tail9)
+            if(!tailMovements.filter(tailMovement=>tailMovement.x==tail9.x && tailMovement.y==tail9.y).length>0){
+                tailMovements.push(tail9)
             }
             // console.log(calculateDistanceBetweenHeadAndTail(head,tail));
         }
@@ -52,14 +68,11 @@ const moveTail = (head, tail)=>{
     }else if((head.x == tail.x-1 && head.y < tail.y-1) || (head.x == tail.x-2 && head.y < tail.y)){
         return {x:tail.x-1, y:tail.y-1}
     }
+    
     else{
         return tail
     }
 
-};
-
-const calculateDistanceBetweenHeadAndTail = (head, tail)=>{
-    return Math.sqrt(Math.pow(head.x-tail.x, 2)+Math.pow(head.y-tail.y, 2))
 };
 
 module.exports = {moveHead}
